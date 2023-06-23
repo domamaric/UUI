a8b2a0989bc4684ce15c9b7801c2d62147397c6b
Domagoj <domamaric20@gmail.com>
2023-06-23 18:52:03 +0200

    Add class comments and sigmoid function

--- lab4/lab4py/nnlearn.py
+++ lab4/lab4py/nnlearn.py
@@ -3,8 +3,7 @@ import numpy as np
 
 
 def load_csv(filepath):
-    """
-    Read csv file and split it data and labels.
+    """Read csv file and split it data and labels.
 
     Truncate first row since it contains only labels and cast all other string
     values to float. Returns 2D matrix as ``np.array`` object and list of
@@ -23,7 +22,25 @@ def load_csv(filepath):
     return np.array(X), y
 
 
+# Activation functions
+def sigmoid(x):
+    """ Calculate sigmoid value of value x. """
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_derivative(x):
+    """ Calculate value derivation of sigmoid function. """
+    return x * (1 - x)
+
+
 class NeuralNetwork:
+    """Neural network class implementation.
+
+    It has an initialization method that takes the dimensions of the input,
+    hidden, and output layers. The weights and biases are randomly initialized
+    from normal distribution with 0.01 standard deviation in the constructor.
+    """
+
     def __init__(self, input_dim, hidden_dims, output_dim):
         self._input_dim = input_dim
         self._hidden_dims = hidden_dims
@@ -41,5 +58,19 @@ class NeuralNetwork:
             )
             self._biases.append(np.random.rand(layer_dims[i + 1]))
 
+    def _forward_propagation(self, X):
+        self.layers = []
+        self.layers.append(X)
+
+        for i in range(len(self.weights)):
+            hidden_output = sigmoid(
+                np.dot(self.layers[i], self.weights[i]) + self.biases[i])
+            self.layers.append(hidden_output)
+
+        return self.layers[-1]
+
     def train():
         pass
+
+    def predict(self, X):
+        return self._forward_propagation(X)
