c361bde17406797e0f201d7d73894fdee35cf0b9
Domagoj <domamaric20@gmail.com>
2023-07-27 15:37:35 +0200

    Add some line preprocessing to C file

--- lab1/lab1py/reader.c
+++ lab1/lab1py/reader.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef void (*LineCallback)(const char *);
+#define MAX_SIZE 512
+
+typedef void (*LineCallback)(const char *a, const char *b);
 
 void read_file_line_by_line(const char *filename, LineCallback callback) {
     FILE *file = fopen(filename, "r");
+
     if (file == NULL) {
-        perror("Error opening file");
+        perror("Error opening file\n");
         return;
     }
 
-    char line[256];
-    while (fgets(line, sizeof(line), file)) {
+    char line_buff[MAX_SIZE];
+    while (fgets(line_buff, MAX_SIZE, file)) {
         // Remove newline character at the end of the line
-        size_t len = strlen(line);
-        if (len > 0 && line[len - 1] == '\n') {
-            line[len - 1] = '\0';
+        size_t len = strlen(line_buff);
+        if (len > 0 && line_buff[len - 1] == '\n') {
+            line_buff[len - 1] = '\0';
         }
 
-        if (line[0] == '#') continue;
+        if (line_buff[0] == '#') continue;
 
-        char *token = strtok(line, " ");
+        // Preprocess the string before passing it to the callback
+        char *colon = strchr(line_buff, ':');
+        if (colon != NULL) {
+            *colon = '\0';  // Replace ':' with '\0' to split the string
+            char *key = line_buff;
+            char *values = colon + 1;
 
-        while (token != NULL) {
-            printf("%s\n", token);
-            token = strtok(NULL, " ");
+            // Call the callback function with the preprocessed line
+            callback(key, values);
         }
-
-        // Call the callback function with the line
-        callback(line);
     }
 
     fclose(file);
